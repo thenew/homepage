8dfbc172fa2e51e334d3e58269649eb3d6cb397b
thenew <remy@thenew.fr>
2012-11-26 00:50:36 +0100

    style

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,15 +2,17 @@ $(document.html).removeClass('no-js').addClass('js');
 
 window.addEvent('domready',function(){
 
-   random_shapes("square", $('bg'), 6, 10);
+    random_shapes("square", $('bg'), 6, 10);
+    $('primary').addClass('liffect-pageLeft');
 
 });
 
 window.addEvent('load',function(){
 
-    document.id('primary').masonry({
-        columnWidth: 110, 
+    $('primary').masonry({
+        columnWidth: 110,
         itemSelector: '.box'
     });
+    $('primary').addClass('play');
 
 });
